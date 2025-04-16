4dfd92ac9040b36e3bc420e860bf92a5ce3f4171
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2025-04-15 19:47:21 -0700

    almost finished the landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,45 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 import BtnCard from '../components/ui/BtnCard';
 import Logo from '../components/ui/Logo';
+//import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
 
 //Definition of the component
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Logo />
-      <BtnCard text="Iniciar Sesión"/>
-    </View>
+  return ( 
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.top}>
+          <Logo />
+        </View>
+        <View style={styles.middle}>
+          <BtnCard text="Iniciar Sesión"/>
+          <BtnCard text="Registrarse"/>
+        </View>
+        <View style={styles.bottom}>
+          <TouchableOpacity>
+            <Text>¿Eres negocio?</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
   );
 }
 
 //Styles
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    flexGrow: 1,
+    justifyContent: "space-between",
     alignItems: "center",
+    paddingVertical: 20,
+    backgroundColor: "#ffffff",
+  },
+  top:{
+    marginTop: 60,
+  },
+  middle:{
+    gap: 20,
+  },
+  bottom:{
+    marginBottom: 30,
   },
 
 })
